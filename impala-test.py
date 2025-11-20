837f863f85b0289d682720b27c49fecee58295b4
richard-vh <69299129+richard-vh@users.noreply.github.com>
2025-11-20 15:55:27 +0000

    Add files via upload

---
+++ impala-test.py
@@ -0,0 +1,21 @@
+import logging
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+ 
+with DAG(
+dag_id="imp_dag",
+start_date=datetime(2025, 2, 9),
+schedule_interval=timedelta(days=1),
+catchup=False,
+) as dag:
+
+  execute_query = SQLExecuteQueryOperator(
+    task_id="execute_query",
+    conn_id="impala-test",
+    sql=f"SHOW TABLES",
+    split_statements=True,
+    return_last=False,
+)
+
+execute_query
