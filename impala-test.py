15b0328d95efb2efbb92920ac2e0d8c580b1d60d
richard-vh <69299129+richard-vh@users.noreply.github.com>
2025-11-21 11:04:42 +0000

    Add files via upload

--- impala-test.py
+++ impala-test.py
@@ -4,24 +4,29 @@ from datetime import datetime, timedelta
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 import pkg_resources
 import importlib.metadata
+
 with DAG(
     dag_id="imp_dag",
     start_date=datetime(2025, 2, 9),
     schedule=timedelta(days=1),
     catchup=False,
 ) as dag:
+    # 🚨 This task definition was missing indentation.
+    execute_query = SQLExecuteQueryOperator(
+        task_id="execute_query",
+        conn_id="rvh-impala",
+        sql=f"SHOW TABLES",
+        split_statements=True,
+        return_last=False,
+    )
 
-execute_query = SQLExecuteQueryOperator(
-    task_id="execute_query",
-    conn_id="rvh-impala",
-    sql=f"SHOW TABLES",
-    split_statements=True,
-    return_last=False,
-)
+    # 🚨 This code was also missing indentation.
+    # Note: Airflow will execute this code when parsing the DAG file, not as a task.
+    installed_packages_list = sorted(["%s==%s" % (d.metadata['Name'], d.version) 
+                                      for d in importlib.metadata.distributions()])
 
-installed_packages_list = sorted(["%s==%s" % (d.metadata['Name'], d.version) 
-                                  for d in importlib.metadata.distributions()])
+    # The print statement will run during the DAG parsing process.
+    print(installed_packages_list)
 
-print(installed_packages_list)
-
-execute_query
+    # 🚨 This task dependency definition was also missing indentation.
+    execute_query
